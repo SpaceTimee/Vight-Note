ec5db9b08a7cbd87ba6919a06eaddab1df48ec52
Space Time <Zeus6_6@163.com>
2021-08-03 16:11:16 +0800

    1.1.8->1.1.9第1次更新

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -21,10 +21,12 @@ namespace Vight_Note
             public const string POLICY_URL = @"https://thoughts.teambition.com/share/609fd36543b2b70046b09b06";
             public const string THOUGHT_PING_URL = @"thoughts.teambition.com";
 
+            #region URL的正则表达式 (URL_REGEX)
             public const string URL_REGEX = //@"^(((ht|f)tp(s?))\://)?" +
                                             @"^([a-zA-Z].)[a-zA-Z0-9\-\.]+\.(" +
-                                            @"com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk|cc|int|arpa|asia|pro|coop|aero|tv|top|xin|xyz|vip|cn|mobi|ru|de|pl|eu|io|jp|club|au|post|me|guru|expert|tw|mo|hk|fr|ar|pk|mv|in|it|ws|sh|my|cd|ac|li|co|cm|win|red|rec|travel|wang|ch|dj|er|ee|es|is|kr|mm|mn|no|ne|to|tr|za|ml|ga|xxx|porn|adult" +
+                                            @"com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk|cc|int|arpa|asia|pro|coop|aero|tv|top|xin|xyz|vip|cn|mobi|ru|de|pl|eu|io|jp|club|au|post|me|guru|expert|tw|mo|hk|fr|ar|pk|mv|in|it|ws|sh|my|cd|ac|li|co|cm|win|red|rec|travel|wang|ch|dj|er|ee|es|is|kr|mm|mn|no|ne|to|tr|za|ml|ga|xxx|porn|adult|cyou|buzz|monster|icu|shop|best|site|live|online|pw|cloud|website|life|store|fun|app|today|uno|space|world|one|link|work|email|nl|trade|bid|stream|men|art|party|date|dev|tech|church|rocks|digital|download|moe|agency|network|click|fail|news|cool|vegas|blog|review|company|la|design|services|golf|exposed|team|ltd|help|zone|loan|wtf|media|solutions|games|directory|center|care|fyi|group|ooo|science|systems|works|city|pet|run|tips|studio|guide|cash|at|support|ninja|plus|rip|marketing|vin|reisen|tools|finance|immo|wiki|viajes|global|mx|promo|recipes|photos|academy|dog|pink|money|chat|casa|cafe|ink|foundation|faith|webcam|house|technology|photography|video|blue|farm|codes|realty|tel|land|show|business|international|social|sbs|skin|page|london|health|hiv|bond|vote|autos|rest|limo|hospital|gay|bar|game|fans|ph|study|cooking|glass|place|rent|shoes|tires|wedding|cab|camp|beer|menu|movie|fish|sexy|gifts|part|mom|green|moda|baby|navy|loans|engineering|computer|camera|barga|film|kitchen|supply|pics|haus|university|fit|cheap|vet|black|law|lol|tax|bio|sale|earth|kim|love|software|fitness|llc|school|pub|deals|style|domains|band|host|direct|shopping|tube|ist|mba|photo|energy|training|taxi|gift|wine|town|bike|toys|ski|poker|yoga|parts|solar|engineer|garden|observer|sucks|hosting|bingo|christmas|gives|horse|insure|diet|fishing|whoswho|tickets|boats|flowers|storage|cfd|inc|quest|luxe|security" +
                                             @")(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\;\?\'\\\+&amp;%\$#\=~_\-]+))*$";
+            #endregion
 
             public const string BAIDU_SEARCH_API = @"https://www.baidu.com/s?wd=";
             public const string BAIDU_TRANSLATE_API = @"https://fanyi.baidu.com/#zh/en/";
@@ -378,7 +380,7 @@ $@"在我的印象里，这似乎是我第一次见到你
             }
             catch
             {
-                Process.Start(Define.BAIDU_SEARCH_API + TextBox.SelectedText);    //百度搜索
+                Process.Start(Define.BAIDU_SEARCH_API + Uri.EscapeDataString(TextBox.SelectedText));    //百度搜索
             }
         }
         private void Translate_Click(object sender, EventArgs e)
