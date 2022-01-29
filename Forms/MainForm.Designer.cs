ee456d542dc9d624a25a34a7a6c9caa975d5053e
Space Time <Zeus6_6@163.com>
2022-01-29 17:42:50 +0800

    1.2.0(beta)->1.2.0第2次更新

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -30,6 +30,7 @@ namespace Vight_Note
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.TextBox = new System.Windows.Forms.TextBox();
             this.FunctionMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.Create = new System.Windows.Forms.ToolStripMenuItem();
@@ -72,18 +73,10 @@ namespace Vight_Note
             this.TextBox.BackColor = System.Drawing.Color.White;
             this.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.TextBox.ContextMenuStrip = this.FunctionMenu;
-            this.TextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextBox.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            resources.ApplyResources(this.TextBox, "TextBox");
             this.TextBox.ForeColor = System.Drawing.Color.Black;
             this.TextBox.HideSelection = false;
-            this.TextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.TextBox.Location = new System.Drawing.Point(0, 0);
-            this.TextBox.Margin = new System.Windows.Forms.Padding(0);
-            this.TextBox.MaxLength = 2147483647;
-            this.TextBox.Multiline = true;
             this.TextBox.Name = "TextBox";
-            this.TextBox.Size = new System.Drawing.Size(404, 438);
-            this.TextBox.TabIndex = 0;
             this.TextBox.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
             this.TextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox_DragDrop);
             this.TextBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox_DragEnter);
@@ -92,7 +85,7 @@ namespace Vight_Note
             // 
             this.FunctionMenu.BackColor = System.Drawing.Color.White;
             this.FunctionMenu.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.FunctionMenu.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            resources.ApplyResources(this.FunctionMenu, "FunctionMenu");
             this.FunctionMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.Create,
             this.Close,
@@ -118,7 +111,6 @@ namespace Vight_Note
             this.FunctionMenu.Name = "TextMenu";
             this.FunctionMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.FunctionMenu.ShowCheckMargin = true;
-            this.FunctionMenu.Size = new System.Drawing.Size(380, 718);
             this.FunctionMenu.Opening += new System.ComponentModel.CancelEventHandler(this.FunctionMenu_Opening);
             this.FunctionMenu.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.FunctionMenu_PreviewKeyDown);
             // 
@@ -127,9 +119,7 @@ namespace Vight_Note
             this.Create.ForeColor = System.Drawing.Color.Black;
             this.Create.Image = global::Vight_Note.Properties.Resources.Create;
             this.Create.Name = "Create";
-            this.Create.ShortcutKeyDisplayString = "(Ctrl+N)";
-            this.Create.Size = new System.Drawing.Size(379, 40);
-            this.Create.Text = "创建窗口";
+            resources.ApplyResources(this.Create, "Create");
             this.Create.Click += new System.EventHandler(this.Create_Click);
             // 
             // Close
@@ -137,25 +127,21 @@ namespace Vight_Note
             this.Close.ForeColor = System.Drawing.Color.Black;
             this.Close.Image = global::Vight_Note.Properties.Resources.Close;
             this.Close.Name = "Close";
-            this.Close.ShortcutKeyDisplayString = "(Ctrl+W)";
-            this.Close.Size = new System.Drawing.Size(379, 40);
-            this.Close.Text = "关闭窗口";
+            resources.ApplyResources(this.Close, "Close");
             this.Close.Click += new System.EventHandler(this.Close_Click);
             // 
             // Separator1
             // 
             this.Separator1.ForeColor = System.Drawing.Color.Black;
             this.Separator1.Name = "Separator1";
-            this.Separator1.Size = new System.Drawing.Size(376, 6);
+            resources.ApplyResources(this.Separator1, "Separator1");
             // 
             // Save
             // 
             this.Save.ForeColor = System.Drawing.Color.Black;
             this.Save.Image = global::Vight_Note.Properties.Resources.Save;
             this.Save.Name = "Save";
-            this.Save.ShortcutKeyDisplayString = "(Ctrl+S)";
-            this.Save.Size = new System.Drawing.Size(379, 40);
-            this.Save.Text = "保存文件";
+            resources.ApplyResources(this.Save, "Save");
             this.Save.Click += new System.EventHandler(this.Save_Click);
             // 
             // Export
@@ -163,9 +149,7 @@ namespace Vight_Note
             this.Export.ForeColor = System.Drawing.Color.Black;
             this.Export.Image = global::Vight_Note.Properties.Resources.Export;
             this.Export.Name = "Export";
-            this.Export.ShortcutKeyDisplayString = "(Alt+S)";
-            this.Export.Size = new System.Drawing.Size(379, 40);
-            this.Export.Text = "导出文件";
+            resources.ApplyResources(this.Export, "Export");
             this.Export.Click += new System.EventHandler(this.Export_Click);
             // 
             // Import
@@ -173,40 +157,34 @@ namespace Vight_Note
             this.Import.ForeColor = System.Drawing.Color.Black;
             this.Import.Image = global::Vight_Note.Properties.Resources.Import;
             this.Import.Name = "Import";
-            this.Import.ShortcutKeyDisplayString = "(Ctrl+O)";
-            this.Import.Size = new System.Drawing.Size(379, 40);
-            this.Import.Text = "导入文件";
+            resources.ApplyResources(this.Import, "Import");
             this.Import.Click += new System.EventHandler(this.Import_Click);
             // 
             // Separator2
             // 
             this.Separator2.ForeColor = System.Drawing.Color.Black;
             this.Separator2.Name = "Separator2";
-            this.Separator2.Size = new System.Drawing.Size(376, 6);
+            resources.ApplyResources(this.Separator2, "Separator2");
             // 
             // Search
             // 
             this.Search.ForeColor = System.Drawing.Color.Black;
             this.Search.Image = global::Vight_Note.Properties.Resources.Search;
             this.Search.Name = "Search";
-            this.Search.ShortcutKeyDisplayString = "(Ctrl+F)";
-            this.Search.Size = new System.Drawing.Size(379, 40);
-            this.Search.Text = "查找文本";
+            resources.ApplyResources(this.Search, "Search");
             this.Search.Click += new System.EventHandler(this.Search_Click);
             // 
             // Separator3
             // 
             this.Separator3.Name = "Separator3";
-            this.Separator3.Size = new System.Drawing.Size(376, 6);
+            resources.ApplyResources(this.Separator3, "Separator3");
             // 
             // ImproveOpacity
             // 
             this.ImproveOpacity.ForeColor = System.Drawing.Color.Black;
             this.ImproveOpacity.Image = global::Vight_Note.Properties.Resources.Improve;
             this.ImproveOpacity.Name = "ImproveOpacity";
-            this.ImproveOpacity.ShortcutKeyDisplayString = "(Ctrl+Alt+U)";
-            this.ImproveOpacity.Size = new System.Drawing.Size(379, 40);
-            this.ImproveOpacity.Text = "透明度+";
+            resources.ApplyResources(this.ImproveOpacity, "ImproveOpacity");
             this.ImproveOpacity.Click += new System.EventHandler(this.ImproveOpacity_Click);
             // 
             // ReduceOpacity
@@ -214,25 +192,21 @@ namespace Vight_Note
             this.ReduceOpacity.ForeColor = System.Drawing.Color.Black;
             this.ReduceOpacity.Image = global::Vight_Note.Properties.Resources.Reduce;
             this.ReduceOpacity.Name = "ReduceOpacity";
-            this.ReduceOpacity.ShortcutKeyDisplayString = "(Ctrl+Alt+D)";
-            this.ReduceOpacity.Size = new System.Drawing.Size(379, 40);
-            this.ReduceOpacity.Text = "透明度-";
+            resources.ApplyResources(this.ReduceOpacity, "ReduceOpacity");
             this.ReduceOpacity.Click += new System.EventHandler(this.ReduceOpacity_Click);
             // 
             // Separator4
             // 
             this.Separator4.ForeColor = System.Drawing.Color.Black;
             this.Separator4.Name = "Separator4";
-            this.Separator4.Size = new System.Drawing.Size(376, 6);
+            resources.ApplyResources(this.Separator4, "Separator4");
             // 
             // AlwaysTop
             // 
             this.AlwaysTop.ForeColor = System.Drawing.Color.Black;
             this.AlwaysTop.Image = global::Vight_Note.Properties.Resources.Top;
             this.AlwaysTop.Name = "AlwaysTop";
-            this.AlwaysTop.ShortcutKeyDisplayString = "(Ctrl+Alt+T)";
-            this.AlwaysTop.Size = new System.Drawing.Size(379, 40);
-            this.AlwaysTop.Text = "置顶窗口";
+            resources.ApplyResources(this.AlwaysTop, "AlwaysTop");
             this.AlwaysTop.Click += new System.EventHandler(this.AlwaysTop_Click);
             // 
             // LockTextBox
@@ -240,9 +214,7 @@ namespace Vight_Note
             this.LockTextBox.ForeColor = System.Drawing.Color.Black;
             this.LockTextBox.Image = global::Vight_Note.Properties.Resources.Unlock;
             this.LockTextBox.Name = "LockTextBox";
-            this.LockTextBox.ShortcutKeyDisplayString = "(Ctrl+Alt+L)";
-            this.LockTextBox.Size = new System.Drawing.Size(379, 40);
-            this.LockTextBox.Text = "锁定输入";
+            resources.ApplyResources(this.LockTextBox, "LockTextBox");
             this.LockTextBox.Click += new System.EventHandler(this.LockTextBox_Click);
             // 
             // DarkMode
@@ -250,9 +222,7 @@ namespace Vight_Note
             this.DarkMode.ForeColor = System.Drawing.Color.Black;
             this.DarkMode.Image = global::Vight_Note.Properties.Resources.Dark;
             this.DarkMode.Name = "DarkMode";
-            this.DarkMode.ShortcutKeyDisplayString = "(Ctrl+Alt+B)";
-            this.DarkMode.Size = new System.Drawing.Size(379, 40);
-            this.DarkMode.Text = "夜间模式";
+            resources.ApplyResources(this.DarkMode, "DarkMode");
             this.DarkMode.Click += new System.EventHandler(this.DarkMode_Click);
             // 
             // ScrollBar
@@ -260,9 +230,7 @@ namespace Vight_Note
             this.ScrollBar.ForeColor = System.Drawing.Color.Black;
             this.ScrollBar.Image = global::Vight_Note.Properties.Resources.Scroll;
             this.ScrollBar.Name = "ScrollBar";
-            this.ScrollBar.ShortcutKeyDisplayString = "(Ctrl+Alt+R)";
-            this.ScrollBar.Size = new System.Drawing.Size(379, 40);
-            this.ScrollBar.Text = "显示滑块";
+            resources.ApplyResources(this.ScrollBar, "ScrollBar");
             this.ScrollBar.Click += new System.EventHandler(this.ScrollBar_Click);
             // 
             // HideBorder
@@ -270,9 +238,7 @@ namespace Vight_Note
             this.HideBorder.ForeColor = System.Drawing.Color.Black;
             this.HideBorder.Image = global::Vight_Note.Properties.Resources.Border;
             this.HideBorder.Name = "HideBorder";
-            this.HideBorder.ShortcutKeyDisplayString = "(Ctrl+Alt+H)";
-            this.HideBorder.Size = new System.Drawing.Size(379, 40);
-            this.HideBorder.Text = "隐藏边框";
+            resources.ApplyResources(this.HideBorder, "HideBorder");
             this.HideBorder.Click += new System.EventHandler(this.OpenBorder_Click);
             // 
             // LiteMode
@@ -280,23 +246,21 @@ namespace Vight_Note
             this.LiteMode.ForeColor = System.Drawing.Color.Black;
             this.LiteMode.Image = global::Vight_Note.Properties.Resources.Lite;
             this.LiteMode.Name = "LiteMode";
-            this.LiteMode.Size = new System.Drawing.Size(379, 40);
-            this.LiteMode.Text = "轻模式";
+            resources.ApplyResources(this.LiteMode, "LiteMode");
             this.LiteMode.Click += new System.EventHandler(this.LiteMode_Click);
             // 
             // toolStripSeparator4
             // 
             this.toolStripSeparator4.ForeColor = System.Drawing.Color.Black;
             this.toolStripSeparator4.Name = "toolStripSeparator4";
-            this.toolStripSeparator4.Size = new System.Drawing.Size(376, 6);
+            resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
             // 
             // Settings
             // 
             this.Settings.ForeColor = System.Drawing.Color.Black;
             this.Settings.Image = global::Vight_Note.Properties.Resources.Settings;
             this.Settings.Name = "Settings";
-            this.Settings.Size = new System.Drawing.Size(379, 40);
-            this.Settings.Text = "设置";
+            resources.ApplyResources(this.Settings, "Settings");
             this.Settings.Click += new System.EventHandler(this.Settings_Click);
             // 
             // Help
@@ -309,16 +273,14 @@ namespace Vight_Note
             this.Help.ForeColor = System.Drawing.Color.Black;
             this.Help.Image = global::Vight_Note.Properties.Resources.Help;
             this.Help.Name = "Help";
-            this.Help.Size = new System.Drawing.Size(379, 40);
-            this.Help.Text = "帮助";
+            resources.ApplyResources(this.Help, "Help");
             // 
             // WhatIsLiteMode
             // 
             this.WhatIsLiteMode.ForeColor = System.Drawing.Color.Black;
             this.WhatIsLiteMode.Image = global::Vight_Note.Properties.Resources.What;
             this.WhatIsLiteMode.Name = "WhatIsLiteMode";
-            this.WhatIsLiteMode.Size = new System.Drawing.Size(291, 44);
-            this.WhatIsLiteMode.Text = "什么是轻模式";
+            resources.ApplyResources(this.WhatIsLiteMode, "WhatIsLiteMode");
             this.WhatIsLiteMode.Click += new System.EventHandler(this.WhatIsLiteMode_Click);
             // 
             // Update
@@ -326,8 +288,7 @@ namespace Vight_Note
             this.Update.ForeColor = System.Drawing.Color.Black;
             this.Update.Image = global::Vight_Note.Properties.Resources.Update;
             this.Update.Name = "Update";
-            this.Update.Size = new System.Drawing.Size(291, 44);
-            this.Update.Text = "检查更新";
+            resources.ApplyResources(this.Update, "Update");
             this.Update.Click += new System.EventHandler(this.Update_Click);
             // 
             // PrivacyPolicy
@@ -335,8 +296,7 @@ namespace Vight_Note
             this.PrivacyPolicy.ForeColor = System.Drawing.Color.Black;
             this.PrivacyPolicy.Image = global::Vight_Note.Properties.Resources.Privacy;
             this.PrivacyPolicy.Name = "PrivacyPolicy";
-            this.PrivacyPolicy.Size = new System.Drawing.Size(291, 44);
-            this.PrivacyPolicy.Text = "隐私政策";
+            resources.ApplyResources(this.PrivacyPolicy, "PrivacyPolicy");
             this.PrivacyPolicy.Click += new System.EventHandler(this.PrivacyPolicy_Click);
             // 
             // About
@@ -344,15 +304,14 @@ namespace Vight_Note
             this.About.ForeColor = System.Drawing.Color.Black;
             this.About.Image = global::Vight_Note.Properties.Resources.About;
             this.About.Name = "About";
-            this.About.Size = new System.Drawing.Size(291, 44);
-            this.About.Text = "关于";
+            resources.ApplyResources(this.About, "About");
             this.About.Click += new System.EventHandler(this.About_Click);
             // 
             // TextMenu
             // 
             this.TextMenu.BackColor = System.Drawing.Color.White;
             this.TextMenu.ImageScalingSize = new System.Drawing.Size(32, 32);
-            this.TextMenu.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            resources.ApplyResources(this.TextMenu, "TextMenu");
             this.TextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.Count,
             this.toolStripSeparator5,
@@ -361,30 +320,26 @@ namespace Vight_Note
             this.TextMenu.Name = "TextMenu";
             this.TextMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.TextMenu.ShowCheckMargin = true;
-            this.TextMenu.Size = new System.Drawing.Size(223, 130);
             // 
             // Count
             // 
             this.Count.ForeColor = System.Drawing.Color.Black;
             this.Count.Image = global::Vight_Note.Properties.Resources.Count;
             this.Count.Name = "Count";
-            this.Count.ShortcutKeyDisplayString = "";
-            this.Count.Size = new System.Drawing.Size(222, 40);
-            this.Count.Text = "字数统计";
+            resources.ApplyResources(this.Count, "Count");
             this.Count.Click += new System.EventHandler(this.Count_Click);
             // 
             // toolStripSeparator5
             // 
             this.toolStripSeparator5.Name = "toolStripSeparator5";
-            this.toolStripSeparator5.Size = new System.Drawing.Size(219, 6);
+            resources.ApplyResources(this.toolStripSeparator5, "toolStripSeparator5");
             // 
             // Run
             // 
             this.Run.ForeColor = System.Drawing.Color.Black;
             this.Run.Image = global::Vight_Note.Properties.Resources.Run;
             this.Run.Name = "Run";
-            this.Run.Size = new System.Drawing.Size(222, 40);
-            this.Run.Text = "智能运行";
+            resources.ApplyResources(this.Run, "Run");
             this.Run.Click += new System.EventHandler(this.Run_Click);
             // 
             // Translate
@@ -392,23 +347,20 @@ namespace Vight_Note
             this.Translate.ForeColor = System.Drawing.Color.Black;
             this.Translate.Image = global::Vight_Note.Properties.Resources.Translate;
             this.Translate.Name = "Translate";
-            this.Translate.Size = new System.Drawing.Size(222, 40);
-            this.Translate.Text = "快捷翻译";
+            resources.ApplyResources(this.Translate, "Translate");
             this.Translate.Click += new System.EventHandler(this.Translate_Click);
             // 
             // MainForm
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(404, 438);
             this.Controls.Add(this.TextBox);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.Black;
             this.KeyPreview = true;
-            this.MinimumSize = new System.Drawing.Size(300, 71);
             this.Name = "MainForm";
             this.Opacity = 0.85D;
             this.ShowIcon = false;
